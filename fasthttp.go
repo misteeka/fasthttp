42b7447fa6a9f87be9082e9b5f12c73b377581bd
misteeka <83140665+misteeka@users.noreply.github.com>
2022-02-26 17:27:30 +0300

    Add Put and Delete, every request is application/json

--- fasthttp.go
+++ fasthttp.go
@@ -7,14 +7,15 @@ func Get(URI string) (resp *fasthttp.Response, err error) {
 	req.SetRequestURI(URI)
 	resp = fasthttp.AcquireResponse()
 	err = fasthttp.Do(req, resp)
+	fasthttp.ReleaseRequest(req)
 	return
 }
 
-func Post(URI string, body []byte, contentType string) (resp *fasthttp.Response, err error) {
+func Post(URI string, body []byte) (resp *fasthttp.Response, err error) {
 	req := fasthttp.AcquireRequest()
 	req.SetBody(body)
 	req.Header.SetMethod("POST")
-	req.Header.SetContentType(contentType)
+	req.Header.SetContentType("application/json")
 	req.SetRequestURI(URI)
 	resp = fasthttp.AcquireResponse()
 	err = fasthttp.Do(req, resp)
@@ -27,21 +28,47 @@ func GetByClient(URI string, client *fasthttp.Client) (resp *fasthttp.Response, 
 	req.SetRequestURI(URI)
 	resp = fasthttp.AcquireResponse()
 	err = client.Do(req, resp)
-	return
-}
-
-func PostByClient(URI string, body []byte, contentType string, client *fasthttp.Client) (resp *fasthttp.Response, err error) {
-	req := fasthttp.AcquireRequest()
-	req.SetBody(body)
-	req.Header.SetMethod("POST")
-	req.Header.SetContentType(contentType)
-	req.SetRequestURI(URI)
-	resp = fasthttp.AcquireResponse()
-	err = client.Do(req, resp)
 	fasthttp.ReleaseRequest(req)
 	return
 }
 
-func PostJSON(URI string, json []byte, postFunc func(string, []byte, string) (resp *fasthttp.Response, err error)) (resp *fasthttp.Response, err error) {
-	return postFunc(URI, json, "application/json")
+func PostByClient(URI string, body []byte, client *fasthttp.Client) (resp *fasthttp.Response, err error) {
+	req := fasthttp.AcquireRequest()
+	req.SetBody(body)
+	req.Header.SetMethod("POST")
+	req.Header.SetContentType("application/json")
+	req.SetRequestURI(URI)
+	resp = fasthttp.AcquireResponse()
+	err = client.Do(req, resp)
+
+	fasthttp.ReleaseRequest(req)
+	return
+}
+
+func Put(URI string, body []byte, contentType string) (resp *fasthttp.Response, err error) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI(URI)
+	req.Header.SetContentType("application/json")
+	req.Header.SetMethodBytes([]byte("PUT"))
+	resp = fasthttp.AcquireResponse()
+	err = fasthttp.Do(req, resp)
+
+	fasthttp.ReleaseRequest(req)
+	return
+}
+
+func Delete(URI string, body []byte, contentType string) (resp *fasthttp.Response, err error) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI(URI)
+	req.Header.SetContentType("application/json")
+	req.Header.SetMethodBytes([]byte("DELETE"))
+	resp = fasthttp.AcquireResponse()
+	err = fasthttp.Do(req, resp)
+
+	fasthttp.ReleaseRequest(req)
+	return
+}
+
+func ReleaseResponse(response *fasthttp.Response) {
+	fasthttp.ReleaseResponse(response)
 }
